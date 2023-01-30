04efec8d63f37367230a5ce0b9589b9a64abcf14
i8lex <cybeerid@gmail.com>
2023-01-30 03:06:11 +0200

    done

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+
+function calculateTeamFinanceReport(salaries, team) {
+
+    const numberOfSpecialists =  team.reduce((acc, { specialization }) => {
+        return (typeof acc[specialization] !== 'undefined')
+            ? {...acc, [specialization]: acc[specialization] + 1}
+            : {...acc, [specialization]: 1}
+    }, {});
+
+    const budgetOfSpecialist = team.reduce((acc, { specialization }  ) => {
+        return (Object.keys(salaries).includes(specialization))
+        ? {...acc, [`totalBudget${specialization}`]: Math.round(numberOfSpecialists[specialization]
+                    * ((salaries[specialization].salary)
+                        + (+salaries[specialization].tax.replace(/%/, "") / 100
+                            * salaries[specialization].salary)))}
+        : {...acc}
+    }, {});
+
+    const budgetOfTeam = Object.values(budgetOfSpecialist).reduce((acc, salary) => {
+        return acc + salary
+    }, 0);
+
+    return {'totalBudgetOfTeam': budgetOfTeam, ...budgetOfSpecialist}
+}
+
+
 const salaries1 = {
     Manager: { salary: 1000, tax: "10%" },
     Designer: { salary: 600, tax: "30%" },
@@ -9,41 +35,24 @@ const team1 = [
     { name: "Vova", specialization: "Designer"},
     { name: "Leo", specialization: "Artist"},]
 
-function calculateTeamFinanceReport(salaries1, team1) {
 
-    const totalBudgetManager = Math.round(team1.filter((position) => position.specialization === "Manager")
-        .length * (salaries1.Manager.salary + (+salaries1.Manager.tax.replace(/%/, "") / 100 * salaries1.Manager.salary)))
 
-    const totalBudgetDesigner = team1.filter((position) => position.specialization === "Designer")
-        .length * (salaries1.Designer.salary + (+salaries1.Designer.tax.replace(/%/, "") / 100 * salaries1.Designer.salary))
-
-    const totalBudgetArtist = team1.filter((position) => position.specialization === "Artist")
-        .length * (salaries1.Artist.salary + (+salaries1.Artist.tax.replace(/%/, "") / 100 * salaries1.Artist.salary))
-
-    const totalBudgetTeam = totalBudgetManager + totalBudgetDesigner + totalBudgetArtist
-
-    return {
-        "totalBudgetTeam": totalBudgetTeam,
-        "totalBudgetManager": totalBudgetManager,
-        "totalBudgetDesigner": totalBudgetDesigner,
-        "totalBudgetArtist": totalBudgetArtist,
-    }
-}
 
 const financeReport1 = calculateTeamFinanceReport(salaries1, team1)
 console.log(JSON.stringify(financeReport1))
 
 
-//
-//
-// const salaries2 = {
-//     TeamLead: { salary: 1000, tax: "99%" },
-//     Architect: { salary: 9000, tax: "34%" },}
-// const team2 = [
-//     { name: "Alexander", specialization: "TeamLead" },
-//     { name: "Gaudi", specialization: "Architect" },
-//     { name: "Koolhas", specialization: "Architect" },
-//     { name: "Foster", specialization: "Architect" },
-//     { name: "Napoleon", specialization: "General" },]
-// const financeReport2 = calculateTeamFinanceReport(salaries2, team2)
-// console.log(JSON.stringify(financeReport2))
+
+
+const salaries2 = {
+    TeamLead: { salary: 1000, tax: "99%" },
+    Architect: { salary: 9000, tax: "34%" },}
+const team2 = [
+    { name: "Alexander", specialization: "TeamLead" },
+    { name: "Gaudi", specialization: "Architect" },
+    { name: "Koolhas", specialization: "Architect" },
+    { name: "Foster", specialization: "Architect" },
+    { name: "Napoleon", specialization: "General" },]
+
+const financeReport2 = calculateTeamFinanceReport(salaries2, team2)
+console.log(JSON.stringify(financeReport2))
